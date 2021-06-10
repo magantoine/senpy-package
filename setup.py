8d97abfdbe03d9dc7a55e3b3728426fef28dd972
Antoine Magron <antoine.magron@epfl.ch>
2021-06-10 15:47:18 +0200

    fills partially setup.py with first info (may have to complete later)

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name="notify-me-senpy", 
+    version="0.1", 
+    description="Ask senpy to notify you on your phone whenever you need on your python script.",
+    author="Abiskorp", 
+    url="https://github.com/magantoine/senpy-package",
+    keywords="PRODUCTIVITY", 
+    license="MIT",
+    classifiers=[
+    'Development Status :: 1 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    ]
+)
