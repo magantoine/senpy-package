8da8ad65cf17808480dd264dca8e19ff5eb9269c
Antoine Magron <antoine.magron@epfl.ch>
2021-06-24 15:41:16 +0200

    adds template for push notif

---
+++ NotifyMeSenpy/senpy.py
@@ -0,0 +1,15 @@
+import request_utils as requ
+
+
+
+def notify_me(content):
+    """
+    send a push notification with given content 
+
+    parameters :
+    content : text content of the push notification
+
+    returns :
+    """
+
+    return None
