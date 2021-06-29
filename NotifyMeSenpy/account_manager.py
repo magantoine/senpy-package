b6daabd2777ed4817466bf9ff43e04e6ced3c8be
Antoine Magron <antoine.magron@epfl.ch>
2021-06-29 11:54:56 +0200

    removes commented lines

--- NotifyMeSenpy/account_manager.py
+++ NotifyMeSenpy/account_manager.py
@@ -52,8 +52,6 @@ def register(on_success, on_failure, tries=200):
             token = res.json()['auth_token']
             user_token.set_token(token)
             user_id = res.json()['id']
-            # print(user_id)
-            #print_success('Account created!')
             on_success(res)
             registered = True
             return Result(Status.SUCCESS, 'Account created')
@@ -71,24 +69,20 @@ def login(on_success, on_failure, tries=200):
         data = prompt(username + password)
         res = post('api/auth/login', data)
         if res.status_code == 200:
-            #print_success("You are logged in, welcome!")
             token = res.json()['auth_token']
             user_token.set_token(token)
             logged_in = True
             on_success(res)
             return Result(Status.SUCCESS, "You are logged in, welcome!")
         else:
-            #print_error(res)
             on_failure(res)
         tries -= 1
     
     return Result(Status.FAILURE, res)
 
 def logout(on_success, on_failure, tries=200):
-    #token = '2882ede988f3961c706dcc39320f4665160a2e67'
     token = user_token.get_token()
     res = post('api/auth/logout', headers={'Authentication': token})
-    #print_success("You are logged out, see you!")
     on_success(res)
     return Result(Status.SUCCESS, "You are logged out, see you!")
     
@@ -111,36 +105,30 @@ def change_password(on_success, on_failure, tries=200):
         }
     ]
     success = False
-    #token = 'Token 2882ede988f3961c706dcc39320f4665160a2e67'
     token = user_token.get_token()
     while not success and tries > 0:
         data = prompt(questions)
         res = post('api/auth/change_password', data, headers={'Authorization':token})
         if res.status_code == 204:
-            #print_success('Password changed successfully.')
             on_success(res)
             success = True
             return Result(Status.SUCCESS, 'Password changed successfully.')
         else:
             on_failure(res)
-            #print_error(res)
         tries -= 1
     return Result(Status.FAILURE, res)
 
 def delete_account(on_success, on_failure, tries=200):
-    #token = 'Token 2882ede988f3961c706dcc39320f4665160a2e67'
     token = user_token.get_token()
     logged_in = False
     while not logged_in and tries > 0:
         data = prompt(username + password)
         res = post('api/auth/delete', data, headers={'Authorization':token})
         if res.status_code == 200:
-            #print_success("Account deleted successfully.")
             on_success(res)
             logged_in = True
             return Result(Status.SUCCESS, "Account deleted successfully.")
         else:
-            #print_error(res)
             on_failure(res)
         tries -= 1
     return Result(Status.FAILURE, res)
