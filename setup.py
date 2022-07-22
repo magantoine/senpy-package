3e6240b9f023dc8769332c4d347a6dc6114b1874
Julien Benhaim <julien.benhaim@mongodb.com>
2022-07-22 16:02:26 +0100

    Fixed versioning and bump version (0.5.0)

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
 from distutils.core import setup
-import setuptools
-from senpy.version import __version__
+
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
 
-
-
 setup(
-    name="notify-me-senpy", # name of the package on PyPi
-    version=__version__, # version number
-    description="Simple python tracker", # short textual description
+    name="notify-me-senpy",  # name of the package on PyPi
+    version="0.5.0",  # version number
+    description="Simple python tracker",  # short textual description
     long_description=readme(),
     long_description_content_type='text/markdown',
-    author="Notify Me SenPy", # authors
+    author="Notify Me SenPy",  # authors
     author_email='notify.me.senpy@gmail.com',
-    url="https://github.com/magantoine/senpy-package", # link to the repo
+    url="https://github.com/magantoine/senpy-package",  # link to the repo
     keywords="PRODUCTIVITY",  # can input a list of descriptive keywords TODO COMPLETE
-    license="GPLv3", # can chose licence TODO COMPLETE
+    license="GPLv3",  # can chose licence TODO COMPLETE
     classifiers=[
-    'Development Status :: 3 - Alpha',      
-    'Intended Audience :: Developers',      
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Programming Language :: Python :: 3',      
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     entry_points={
         'console_scripts': [
@@ -40,7 +38,7 @@ setup(
         # "PyInquirer==1.0.3",
         "requests==2.26.0",
         "inquirerpy==0.3.0"
-      ],
+    ],
     packages=["senpy"],
     # link to the release
     # download_url="https://github.com/magantoine/senpy-package/archive/refs/tags/0.5.tar.gz"
